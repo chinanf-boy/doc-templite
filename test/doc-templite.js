2477a518903db3ef256c4daa113c2eb3aeafc841
chinanf-boy <865501259@qq.com>
2018-08-19 14:49:53 +0800

    test: 💍 more details

--- test/doc-templite.js
+++ test/doc-templite.js
@@ -8,9 +8,12 @@ const Regex = /yobrave \| 18/
 
 test.todo('doc-templite')
 
+const getPath = (p) => path.resolve(testDir,p)
+const getF = (p) => fs.readFileSync(p,'utf8')
+
 test('file: no doc-templite tag', t => {
-    const p = path.resolve(testDir,'./features/no-doc-templite.md')
-    const f = fs.readFileSync(p,'utf8');
+    const p = getPath('./features/no-doc-templite.md')
+    const f = getF(p)
     let opts = {
         path:p,
         templite,
@@ -18,13 +21,13 @@ test('file: no doc-templite tag', t => {
     let result = doc(f,opts)
     let {transformed,data,toml} = result
 
-    t.is(Object.keys(toml).length,0);
+	t.is(toml.length,0);
     t.false(transformed);
     t.is(data,f)
 });
 test('each toml with single remark', t => {
-    const p = path.resolve(testDir,'./features/first-test.md')
-    const f = fs.readFileSync(p,'utf8');
+    const p = getPath('./features/first-test.md')
+    const f = getF(p)
     let opts = {
         path:p,
         templite,
@@ -32,14 +35,15 @@ test('each toml with single remark', t => {
     let result = doc(f,opts)
     let {transformed,data,toml} = result
 
-    t.is(Object.keys(toml).length,1);
+	t.is(toml.length,1);
+	t.is(toml[0].age,18);
     t.true(transformed);
     t.regex(data,Regex)
 });
 
 test('one remark with tomls ', t => {
-    const p = path.resolve(testDir,'./features/one-remark-tomls.md')
-    const f = fs.readFileSync(p,'utf8');
+    const p = getPath('./features/one-remark-tomls.md')
+    const f = getF(p)
     let opts = {
         path:p,
         templite,
@@ -47,17 +51,50 @@ test('one remark with tomls ', t => {
     let result = doc(f,opts)
     let {transformed,data,toml} = result
 
-    t.is(Object.keys(toml).length,1);
+	t.is(toml.length,1);
+	t.is(toml[0].age,18);
     t.true(transformed);
     t.regex(data,Regex)
 });
 
-test('more doc-templite tag', t => {
-	const p = path.resolve(testDir,'./features/more-doc-templite-tag.md')
-    const genPath = path.resolve(testDir,'./features/gen-more-doc-templite-tag.md')
+test('one tag with one mulit remark tomls ', t => {
+    const p = getPath('./features/one-tag-one-mulit-remark.md')
+    const f = getF(p)
+    let opts = {
+        path:p,
+        templite,
+    }
+    let result = doc(f,opts)
+    let {transformed,data,toml} = result
+	t.is(toml.length,1);
+	t.is(toml[0].age,18);
 
-	const f = fs.readFileSync(p,'utf8');
-    const gen = fs.readFileSync(genPath,'utf8');
+    t.true(transformed);
+    t.regex(data,Regex)
+});
+
+test('more doc-templite tag Same content', t => {
+    const p = getPath('./features/gen-more-doc-templite-tag.md')
+    const f = getF(p)
+    let opts = {
+        path:p,
+        templite,
+    }
+    let result = doc(f,opts)
+    let {transformed,data,toml} = result
+
+	t.is(toml.length,2);
+	t.is(toml[0].age,18);
+    t.false(transformed);
+    t.is(data,f)
+});
+
+test('more doc-templite tag', t => {
+	const p = getPath('./features/more-doc-templite-tag.md')
+    const genPath = getPath('./features/gen-more-doc-templite-tag.md')
+
+	const f = getF(p)
+    const gen = getF(genPath)
 
     let opts = {
         path:p,
@@ -66,7 +103,8 @@ test('more doc-templite tag', t => {
     let result = doc(f,opts)
     let {transformed,data,toml} = result
 
-    t.is(Object.keys(toml).length,2);
+	t.is(toml.length,2);
+	t.is(toml[0].age,18);
     t.true(transformed);
     t.is(data,gen)
 });
